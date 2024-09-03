3b7520cc3001a1015570acc5c5db3cadffc4de06
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-03 08:01:25 -0300

    add new structure to global infos and prepare superheader

--- src/components/FloatWppButton/index.tsx
+++ src/components/FloatWppButton/index.tsx
@@ -1,9 +1,9 @@
-import { infos } from "@/utils/constants/infos"
+import { whatsApp } from "@/utils/constants/infos"
 import { BsWhatsapp } from "react-icons/bs"
 
 export const FloatWppButton = () => {
   return (
-    <a href={infos.whatsAppUrl} target='_blank' className='fixed bottom-4 right-4'>
+    <a href={whatsApp.url} target='_blank' className='fixed bottom-4 right-4'>
       <div className='bg-green-wpp p-4 w-fit rounded-full text-white cursor-pointer'>
         <BsWhatsapp className='text-4xl'/>
       </div>
---
+++ src/components/Header/SuperHeader.tsx
@@ -0,0 +1,15 @@
+import { ScreenWidthLimiter } from '@/components/ScreenWidthLimiter'
+import React from 'react'
+
+export const SuperHeader = () => {
+  return (
+    <div>
+      <ScreenWidthLimiter>
+        <div className="flex items-center">
+          
+        </div>
+        <div className="flex items-center"></div>
+      </ScreenWidthLimiter>
+    </div>
+  )
+}
