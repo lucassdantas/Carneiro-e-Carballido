209e586463a6bfc91621c36e19d57bf379e69473
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-25 17:44:40 -0300

    fixing banner on home

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -6,55 +6,58 @@ import { FaMapMarkerAlt } from 'react-icons/fa';
 import { SuperTitle } from '@/components/Titles/SuperTitle';
 import { ContactForm } from '@/components/ContactForm';
 import { Hero } from '@/components/Hero';
-import { ScreenWidthLimiter } from '@/components/ScreenWidthLimiter';
+import { Section } from '@/components/Section';
+import contabilityGrapich from '@/assets/graficos-de-contabilidade-cec-consultores.jpg'
 
 export const Contact = () => {
   
   return (
     <Template pageTitle='Contato'>
       <Hero pageTitle='Contato'/>
-      <ScreenWidthLimiter>
-        <section className='flex py-12 items-center gap-12'>
-          <div className="flex flex-col w-1/2">
-            <SuperTitle content='Continue conectado conosco' className='mb-2'/>
-            <h2 className='text-4xl mb-4'>Fale conosco para uma solução personalizada</h2>
-            <p className='mb-4'>Lorem ipsum</p>
-            <ul className='flex flex-col gap-2 items-center md:items-start max-w-[60%]'>
-              <li>
-                <a href={email.url} className='flex gap-2 items-center'>
-                  <MdEmail className='text-lg' />
-                  {email.value}
-                </a>
-              </li>
-              <li>
-                <a href={whatsApp.url} className='flex gap-2 items-center'>
-                  <BsWhatsapp className='text-lg' />
-                  {whatsApp.value}
-                </a>
-              </li>
-              <li>
-                <a href={address.url} className='flex gap-2 items-center'>
-                  <FaMapMarkerAlt className='text-2xl' />
-                  {address.value}
-                </a>
-              </li>
-            </ul>
+        <Section className='py-12 '>
+          <div className="flex items-center gap-12">
+            <div className="flex flex-col w-1/2">
+              <SuperTitle content='Continue conectado conosco' className='mb-2'/>
+              <h2 className='text-4xl mb-4'>Fale conosco para uma solução personalizada</h2>
+              <p className='mb-4'>Lorem ipsum</p>
+              <ul className='flex flex-col gap-2 items-center md:items-start max-w-[60%]'>
+                <li>
+                  <a href={email.url} className='flex gap-2 items-center'>
+                    <MdEmail className='text-lg' />
+                    {email.value}
+                  </a>
+                </li>
+                <li>
+                  <a href={whatsApp.url} className='flex gap-2 items-center'>
+                    <BsWhatsapp className='text-lg' />
+                    {whatsApp.value}
+                  </a>
+                </li>
+                <li>
+                  <a href={address.url} className='flex gap-2 items-center'>
+                    <FaMapMarkerAlt className='text-2xl' />
+                    {address.value}
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex flex-col w-1/2 items-center">
+              <img src={contabilityGrapich} className="bg-gray-700 h-[450px] w-full rounded-xl object-cover"/>
+            </div>
           </div>
-          <div className="flex flex-col w-1/2 items-center">
-            <div className="bg-gray-700 h-[450px] w-full rounded-xl"></div>
+        </Section>
+        <Section className='py-12'>
+          <div className="flex items-center  gap-12">
+            <div className="flex flex-col w-1/2 items-center">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4397.941197483763!2d-43.18408402390271!3d-22.90157463768632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997f60863134a1%3A0xfaab42b35519cdd6!2sCarneiro%20e%20Carballido%20Contabilidade!5e1!3m2!1sen!2sbr!4v1725422607947!5m2!1sen!2sbr" width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div className="flex flex-col w-1/2">
+              <h2 className='text-4xl mb-4'>Envie uma mensagem</h2>
+              <p className='mb-4'>Lorem ipsum</p>
+              <ContactForm/>
+            </div>
           </div>
-        </section>
-        <section className='flex py-12 items-center gap-12'>
-          <div className="flex flex-col w-1/2 items-center">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4397.941197483763!2d-43.18408402390271!3d-22.90157463768632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997f60863134a1%3A0xfaab42b35519cdd6!2sCarneiro%20e%20Carballido%20Contabilidade!5e1!3m2!1sen!2sbr!4v1725422607947!5m2!1sen!2sbr" width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-          </div>
-          <div className="flex flex-col w-1/2">
-            <h2 className='text-4xl mb-4'>Envie uma mensagem</h2>
-            <p className='mb-4'>Lorem ipsum</p>
-            <ContactForm/>
-          </div>
-        </section>
-      </ScreenWidthLimiter>
+        </Section>
     </Template>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,5 @@
 import { Template } from '@/components/Template'
 import manImage from '@/assets/man-sitting-accessing-his-laptop.jpg';
-import accountingService from '@/assets/image-accounting-service.jpg'
-import businessService from '@/assets/business-and-tax-consultancy-service-image.jpg'
-import companiesService from '@/assets/service-image-opening-and-legalization-of-companies.jpg'
 import googleLogo from '@/assets/Google-Logo.png'
 import banner from '@/assets/banner.jpg'
 import aboutImg from '@/assets/about-image.jpg'
@@ -12,98 +9,95 @@ import { FAQ } from '@/components/FAQ';
 import { TestimonialCarousel } from '@/components/TestimonialCarousel';
 import { Section } from '@/components/Section';
 import { services } from '@/utils/services';
+import { SuperTitle } from '@/components/Titles/SuperTitle';
 
 
 
 export const Home = () => {
   return (
     <Template pageTitle="Home" transparentHeader={true}>
-      <section
-        className=" h-screen p-40 bg-no-repeat bg-cover  flex gap-10 items-center  "
-        style={{ backgroundImage: `url(${manImage})`  }} 
-      >
+      <Section className="h-screen p-40 bg-no-repeat bg-cover flex gap-10 items-center" bgImg={manImage} >
         <div className='flex flex-col gap-10'>
-          <div className='w-4/12'>
-        <h1 className="text-white text-5xl font-semibold">
-          Contabilidade da sua 
-          empresa de forma prática, segura e econômica
-
-        </h1>
+          <div className='w-1/2 text-white'>
+            <SuperTitle content='Cec Consultores' color='white' className='mb-8'/>
+            <h1 className="text-white text-4xl font-bold my-4">Contabilidade da sua empresa de forma prática, segura e econômica</h1>
+            <p>Tenha a praticidade de uma contabilidade digital com a credibilidade de um atendimento humanizado.</p>
+          </div>
+          <div className='w-64'>
+            <Button content='Entre em contato'/>
+          </div>
         </div>
-        <div className='w-64'>
-        <Button content='Entre em contato'/>
+      </Section>
+
+      <Section className='relative flex flex-col items-center justify-center py-10 '>
+        <div className='text-center'>
+          <h2 className='text-4xl text-dark-blue font-semibold'>Nossos serviços</h2>
         </div>
+        <div className='flex gap-10 mt-10'>
+          {services.map((service, i) => {
+            if(i>2) return null
+            return <ServicesCard key={i} serviceTitle={service.title} imgUrl={service.img}>{service.resume}</ServicesCard>
+          })}
         </div>
-      </section>
-        <Section className='relative flex flex-col items-center justify-center py-10 '>
-          <div className='text-center'>
-            <h2 className='text-4xl text-dark-blue font-semibold'>Nossos serviços</h2>
-          </div>
-          <div className='flex gap-10 mt-10'>
-            {services.map((service, i) => {
-              if(i>2) return null
-              return <ServicesCard key={i} serviceTitle={service.title} imgUrl={service.img}>{service.resume}</ServicesCard>
-            })}
-          </div>
-          <div className='mt-12 w-full text-center px-[25%]'>
-            <Button url={'/servicos'} content='Saiba mais' target='' />
-          </div>
-        </Section>
+        <div className='mt-12 w-full text-center px-[25%]'>
+          <Button url={'/servicos'} content='Saiba mais' target='' />
+        </div>
+      </Section>
 
-        <Section
-          className="businessBg w-full h-72 p-10 bg-fixed bg-cover bg-no-repeat bg-center flex items-center justify-center"
-          bgImg={banner}
-        >
-          <div className="text-center">
-            <h2 className="text-white text-4xl font-semibold ">Melhorar o seu negócio</h2>
-            <p className="text-white text-2xl ">Profissionais qualificados e preparados para trazer as melhores soluções.</p>
-          </div>
+      <Section
+        className="businessBg w-full h-72 p-10 bg-fixed bg-cover bg-no-repeat bg-center flex items-center justify-center"
+        bgImg={banner}
+      >
+        <div className="text-center">
+          <h2 className="text-white text-4xl font-semibold ">Melhorar o seu negócio</h2>
+          <p className="text-white text-2xl ">Profissionais qualificados e preparados para trazer as melhores soluções.</p>
+        </div>
 
-        </Section>
+      </Section>
 
-        <Section className='flex py-12 items-center gap-12'>
-          <div className='flex py-12 items-center gap-12'>
-            <div className="flex flex-col w-1/2 items-start">
-              <h2 className='text-4xl mb-4  text-dark-blue font-semibold'>Sobre a empresa</h2>
-              <p className='mb-4'>A Carneiro e Carballido oferece soluções contábeis personalizadas para crescimento e eficiência financeira, com foco em contabilidade empresarial, fiscal, trabalhista e consultoria tributária.</p>
-              <Button/>
-            </div>
-            <div className="flex flex-col w-1/2 items-center">
-              <img className='w-full h-[512px] object-cover rounded-lg shadow-lg' src={aboutImg} alt="Imagem sobre a empresa" />
-            </div>
+      <Section className='flex py-12 items-center gap-12'>
+        <div className='flex py-12 items-center gap-12'>
+          <div className="flex flex-col w-1/2 items-start">
+            <h2 className='text-4xl mb-4  text-dark-blue font-semibold'>Sobre a empresa</h2>
+            <p className='mb-4'>A Carneiro e Carballido oferece soluções contábeis personalizadas para crescimento e eficiência financeira, com foco em contabilidade empresarial, fiscal, trabalhista e consultoria tributária.</p>
+            <Button/>
           </div>
-        </Section>
+          <div className="flex flex-col w-1/2 items-center">
+            <img className='w-full h-[512px] object-cover rounded-lg shadow-lg' src={aboutImg} alt="Imagem sobre a empresa" />
+          </div>
+        </div>
+      </Section>
 
 
-        <Section className='bg-light-gray py-10'>
-          <h2 className="text-4xl  text-center mb-6 font-semibold">Perguntas Frequentes</h2>
-          <FAQ/>
-        </Section>
+      <Section className='bg-light-gray py-10'>
+        <h2 className="text-4xl  text-center mb-6 font-semibold">Perguntas Frequentes</h2>
+        <FAQ/>
+      </Section>
 
-        <Section className='flex flex-col items-center gap-5 w-full py-10'>
-          <h2 className='text-center text text-4xl  text-dark-blue font-semibold'> O que nossos clientes falam</h2>
-          <h3 className='text-2xl font-semibold'>Excelente</h3>
-          <div className="flex">
-            <svg className={`h-5 w-5 text-yellow-500`} fill="currentColor">
-              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-            </svg>
-            <svg className={`h-5 w-5 text-yellow-500`} fill="currentColor">
-              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-            </svg>
-            <svg className={`h-5 w-5 text-yellow-500`} fill="currentColor">
-              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-            </svg>
-            <svg className={`h-5 w-5 text-yellow-500`} fill="currentColor">
-              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-            </svg>
-            <svg className={`h-5 w-5 text-yellow-500`} fill="currentColor">
-              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-            </svg>
-          </div>
-          <p>Com base em 5 avaliações</p>
-          <img className='w-24' src={googleLogo} alt="Google Logo" />
-          <TestimonialCarousel/>
-        </Section>
+      <Section className='flex flex-col items-center gap-5 w-full py-10'>
+        <h2 className='text-center text text-4xl  text-dark-blue font-semibold'> O que nossos clientes falam</h2>
+        <h3 className='text-2xl font-semibold'>Excelente</h3>
+        <div className="flex">
+          <svg className={`h-5 w-5 text-yellow-500`} fill="currentColor">
+            <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+          </svg>
+          <svg className={`h-5 w-5 text-yellow-500`} fill="currentColor">
+            <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+          </svg>
+          <svg className={`h-5 w-5 text-yellow-500`} fill="currentColor">
+            <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+          </svg>
+          <svg className={`h-5 w-5 text-yellow-500`} fill="currentColor">
+            <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+          </svg>
+          <svg className={`h-5 w-5 text-yellow-500`} fill="currentColor">
+            <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+          </svg>
+        </div>
+        <p>Com base em 5 avaliações</p>
+        <img className='w-24' src={googleLogo} alt="Google Logo" />
+        <TestimonialCarousel/>
+      </Section>
     </Template>
   )
 }
