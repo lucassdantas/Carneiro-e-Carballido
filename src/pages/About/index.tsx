d733080e10e875c16daf4bf118644c92a1c178ff
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-28 02:45:23 -0300

    change about images

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,13 +1,14 @@
 import { Template } from '@/components/Template'
-import ourTeam from '@/assets/our-team.jpg'
-import team from '@/assets/team.jpg'
 import { Hero } from '@/components/Hero';
 import { Section } from '@/components/Section';
+import tempBanner01 from '@/assets/_img/departamento.jpg'
+import tempBanner02 from '@/assets/_img/consultoria.jpg'
+
 
 export const About = () => {
   return (
-    <Template pageTitle='Sobre'>
-      <Hero pageTitle='Sobre'/>
+    <Template pageTitle='Conheça a CeC'>
+      <Hero pageTitle='Conheça a CeC'/>
         <Section className='py-12 gap-12'>
           <h1 className='text-dark-yellow'>Nós somos a Carneiro & Carbalido</h1>
           <div className='flex lg:flex-row flex-col items-center gap-28'>
@@ -15,8 +16,8 @@ export const About = () => {
             <p>Um negócio de sucesso exige estratégia, táticas eficazes e decisões equilibradas. Confie em nossos serviços e foque no crescimento da sua empresa</p>
           </div>
           <div className='flex lg:flex-row flex-col items-center gap-20 lg:mt-4'>
-            <img className='w-full rounded-lg shadow-lg object-cover' src={ourTeam} alt="Imagem sobre a empresa" />
-            <img className='w-full rounded-lg shadow-lg object-cover' src={team} alt="Imagem sobre a empresa" />
+            <img className='w-full rounded-lg shadow-lg object-cover' src={tempBanner01} alt="Imagem sobre a empresa" />
+            <img className='w-full rounded-lg shadow-lg object-cover' src={tempBanner02} alt="Imagem sobre a empresa" />
           </div>
           <div className='flex flex-col gap-10 lg:my-4'>
             <h2 className='text-4xl text-dark-blue font-semibold'>Nossa Equipe</h2>
